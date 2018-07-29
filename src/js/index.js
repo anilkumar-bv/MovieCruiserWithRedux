dd18c39c079adf6fc09ebee87ac710a05661d5cb
Anil Kumar <anilkumar_bv@yahoo.com>
2018-07-29 22:52:29 +0530

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import { loadInitialPage} from './views';
+import { clearBox } from './services';
+import {filterItems} from './controllers';
+
+loadInitialPage();
+
+document.getElementById('search').addEventListener('click', filterItems);
+
+document.getElementById('backButton').addEventListener('click', () => {
+
+    // clear the Contents of the Main div Tag
+    clearBox('contentDiv');
+
+    // Load Home Page
+    loadInitialPage();
+});
